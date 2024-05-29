5e5464c083688788e4a94dddfcf970fd9c87cedf
Srijan kumar <Srijan kumar>
2024-05-29 22:50:27 +0530

    modifucation  Commit

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 document.querySelector("#slider").addEventListener("click",()=>{
     if(document.querySelector("#groove").style.justifyContent=="flex-start"){
         document.querySelector("#groove").style.justifyContent="flex-end";
-        document.querySelector("body").style.backgroundColor="white";
-        document.querySelector("#slider").style.backgroundColor="white";
-        document.querySelector("#groove").style.backgroundColor="black";
+        document.querySelector("body").style.backgroundColor="black";
+        document.querySelector("#slider").style.backgroundColor="black";
+        document.querySelector("#groove").style.backgroundColor="white";
 
     }else{
         document.querySelector("#groove").style.justifyContent="flex-start";
-        document.querySelector("body").style.backgroundColor="black";
-        document.querySelector("#slider").style.backgroundColor="black";
-        document.querySelector("#groove").style.backgroundColor="white"
+        document.querySelector("body").style.backgroundColor="white";
+        document.querySelector("#slider").style.backgroundColor="white";
+        document.querySelector("#groove").style.backgroundColor="black"
     }
     
 });
