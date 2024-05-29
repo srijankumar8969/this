7adcddfcdba9c4277e74da40269aa515bd0c061a
Srijan kumar <Srijan kumar>
2024-05-29 22:01:11 +0530

    ToggleDarkMode Commited

---
+++ index.js
@@ -0,0 +1,16 @@
+
+document.querySelector("#slider").addEventListener("click",()=>{
+    if(document.querySelector("#groove").style.justifyContent=="flex-start"){
+        document.querySelector("#groove").style.justifyContent="flex-end";
+        document.querySelector("body").style.backgroundColor="white";
+        document.querySelector("#slider").style.backgroundColor="white";
+        document.querySelector("#groove").style.backgroundColor="black";
+
+    }else{
+        document.querySelector("#groove").style.justifyContent="flex-start";
+        document.querySelector("body").style.backgroundColor="black";
+        document.querySelector("#slider").style.backgroundColor="black";
+        document.querySelector("#groove").style.backgroundColor="white"
+    }
+    
+});
